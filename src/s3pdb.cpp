db8ede43d9f9fe078155df6acd1fa6b1389e8c7a
Mark Harmstone <mark@harmstone.com>
2021-03-10 23:55:29 +0000

    solicit filenames

--- src/s3pdb.cpp
+++ src/s3pdb.cpp
@@ -5,27 +5,20 @@
 #include <aws/s3/model/PutObjectRequest.h>
 #include <aws/core/auth/AWSCredentialsProvider.h>
 #include <string>
+#include <filesystem>
 
 using namespace std;
 
 static const string_view access_key = "AKIAXYIHB2JMQD2MMCVX"; // FIXME
 static const string_view secret_key = "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"; // FIXME
 
-static void upload(const string_view& bucket, const string& filename, const string_view& region) {
-    Aws::Client::ClientConfiguration config;
-
-    config.region = Aws::String(region);
-
-    Aws::Auth::AWSCredentials credentials;
-
-    credentials.SetAWSAccessKeyId(Aws::String(access_key));
-    credentials.SetAWSSecretKey(Aws::String(secret_key));
-
-    Aws::S3::S3Client s3_client(credentials, config);
+static void upload(Aws::S3::S3Client& s3_client, const string_view& bucket, const filesystem::path& filename) {
+    if (!exists(filename))
+        throw runtime_error(filename.u8string() + " does not exist.");
 
     Aws::S3::Model::PutObjectRequest request;
     request.SetBucket(Aws::String(bucket));
-    request.SetKey(Aws::String(filename));
+    request.SetKey(Aws::String(filename.filename().u8string())); // FIXME - prepend GUID etc.
 
     auto input_data = make_shared<fstream>(filename, ios_base::in | ios_base::binary);
 
@@ -37,16 +30,46 @@ static void upload(const string_view& bucket, const string& filename, const stri
         throw runtime_error("S3 PutObject failed: " + string(outcome.GetError().GetMessage()));
 }
 
-int main() {
+int main (int argc, char* argv[]) {
+    bool failed = false;
+
+    if (argc < 2) {
+        fprintf(stderr, "Usage: s3pdb <pdb-file>\n");
+        return 1;
+    }
+
     try {
         Aws::SDKOptions options;
         Aws::InitAPI(options);
 
         string bucket_name = "symbols.burntcomma.com"; // FIXME
-        string object_name = "my-file.txt"; // FIXME
         string region = "eu-west-2"; // FIXME
 
-        upload(bucket_name, object_name, region);
+        Aws::Client::ClientConfiguration config;
+
+        config.region = Aws::String(region);
+
+        Aws::Auth::AWSCredentials credentials;
+
+        credentials.SetAWSAccessKeyId(Aws::String(access_key));
+        credentials.SetAWSSecretKey(Aws::String(secret_key));
+
+        Aws::S3::S3Client s3_client(credentials, config);
+
+        for (int i = 1; i < argc; i++) {
+            bool success = false;
+
+            try {
+                upload(s3_client, bucket_name, argv[i]);
+                success = true;
+            } catch (const exception& e) {
+                fprintf(stderr, "Error uploading %s: %s\n", argv[i], e.what());
+                failed = true;
+            }
+
+            if (success)
+                printf("Uploaded %s.\n", argv[i]);
+        }
 
         Aws::ShutdownAPI(options);
     } catch (const exception& e) {
@@ -54,5 +77,5 @@ int main() {
         return 1;
     }
 
-    return 0;
+    return failed ? 1 : 0;
 }
